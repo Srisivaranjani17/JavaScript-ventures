b9c3969c439184a9e2a85c256ef56eecba175f95
Srisivaranjani17 <srisivaranjanim@gmail.com>
2025-06-14 19:49:56 +0530

    feat:Add second project

---
+++ weather App/script.js
@@ -0,0 +1,33 @@
+const btn=document.getElementById('getWeather');
+const cityInput=document.getElementById('city');
+const Cityname=document.getElementById('cityname');
+const temperature=document.getElementById('temperature');
+const description=document.getElementById('description');
+
+
+btn.addEventListener("click",()=>{
+    const city=cityInput.value;
+    const apiKey="c76efbb811a4cfeb4fff420c6f51ca56";
+
+    if(city){
+        const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+        fetch(url)
+        .then(response=>response.json())
+        .then(data=>{
+            if(data.cod === '404'){
+                alert('City not found!');
+            }else{
+                Cityname.textContent=data.name;
+                temperature.textContent=`${data.main.temp}°C`;
+                description.textContent=data.weather[0].description;
+            }
+        })
+        .catch(error=>{
+            alert("Error fetching data!");
+        });
+    }else{
+        alert("please enter a city!");
+    }
+    
+})
